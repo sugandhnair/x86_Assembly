2820b05d8d0056773892ff4c060601533662879b
SUGANDH D NAIR <61353024+sugandhnair@users.noreply.github.com>
2021-09-18 15:59:48 +0530

    Update basic_template.asm
    Added x64 and 16 bit templates.

--- basic_template.asm
+++ basic_template.asm
@@ -1,10 +1,41 @@
-.386
-.model	flat
+;--------------- x64 - 64 bit -------------------------- 
 .code
 main	PROC
-	mov	eax,213
-	add 	eax,432
+	mov	rax,2
+	mov rbx,3
+	mov rcx,4
+	ret
+main	endp
+end
 
+;-------------------------------------------------------
+
+;--------------------- x86 - 32 bit----------------------
+
+.386
+.model flat 
+.code
+main	PROC
+	mov	eax,2
+	mov ebx,3
+	mov ecx,4
 	ret
 main	endp
 end	main
+
+;---------------------------------------------------------
+
+;=------------------------16bit---------------------------
+
+.386
+.model flat 
+.code
+main	PROC
+	mov	ax,2
+	mov bx,3
+	mov cx,4
+	ret
+main	endp
+end	main
+
+------------------------------------------------------------
