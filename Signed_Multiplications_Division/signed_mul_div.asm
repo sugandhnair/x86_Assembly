5e8a0ed9b34d4ac13d1b4c8a94d959257e317d40
SUGANDH D NAIR <61353024+sugandhnair@users.noreply.github.com>
2021-09-22 12:34:27 +0530

    Create signed_mul_div.asm

---
+++ Signed_Multiplications_Division/signed_mul_div.asm
@@ -0,0 +1,47 @@
+.386
+.model flat,c
+.code
+
+;Return : 0 Error (Division by zero)
+;		: 1 Success
+;
+;Computation *prod = a*b
+;			 *quo = a/b
+;			 *rem = a %b
+
+IntegerMulDiv proc
+				
+			 push ebp
+			 mov ebp,esp
+			 push ebx
+
+			 xor eax,eax
+
+			 mov ecx,[ebp+8]		;ecx ='a'
+			 mov edx,[ebp+12]		;edx ='b'
+
+			 or edx, edx
+			 jz InvalidDivisor
+
+			 imul edx,ecx   ;edx = 'a'*'b'
+
+			 mov ebx,[ebp+16]  ; ebx ='prod'
+			 mov [ebx],edx
+
+			 mov eax,ecx			;eax ='a'
+			 cdq					;edx:eax contains dividend
+
+			idiv dword ptr[ebp+12]
+
+			mov ebx,[ebp+20]
+			mov [ebx],eax
+			mov ebx,[ebp+24]
+			mov [ebx],edx
+			mov eax,1
+
+	InvalidDivisor:
+			pop ebx
+			pop ebp
+			ret
+IntegerMulDiv  endp
+			end
